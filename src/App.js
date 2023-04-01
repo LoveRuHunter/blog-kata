fa2d42990f7423b30688ee7938e40a8cb3dd070a
LoveRuHunter <113541018+LoveRuHunter@users.noreply.github.com>
2023-04-01 13:30:52 +0500

    update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { CreateAccountPage } from './pages/create-account-page/CreateAccountPage
 import { LoginPage } from './pages/login-page/LoginPage'
 import { EditAccountPage } from './pages/edit-account-page/EditAccountPage'
 import { SingleArticlePage } from './pages/single-article-page/SingleArticlePage'
-import { CreateArticlePage} from './pages/create-article-page/CreateArticlePage'
-import { EditArticlePage} from './pages/edit-article-page/EditArticlePage'
+import { CreateArticlePage } from './pages/create-article-page/CreateArticlePage'
+import { EditArticlePage } from './pages/edit-article-page/EditArticlePage'
+import { route } from "./helpers/helpers";
 
 import { Layout } from './components/layout/Layout'
 
@@ -15,12 +16,12 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />}/>
-        <Route path="create-account" element={<CreateAccountPage />}/>
-        <Route path="login" element={<LoginPage />}/>
-        <Route path="edit-account" element={<EditAccountPage />}/>
-        <Route path="/:id" element={<SingleArticlePage />}/>
-        <Route path="create-article" element={<CreateArticlePage />}/>
-        <Route path="edit-article/:id" element={<EditArticlePage />}/>
+        <Route path={route.createAccount} element={<CreateAccountPage />}/>
+        <Route path={route.Login} element={<LoginPage />}/>
+        <Route path={route.editAccount} element={<EditAccountPage />}/>
+        <Route path={route.Id} element={<SingleArticlePage />}/>
+        <Route path={route.createArticle} element={<CreateArticlePage />}/>
+        <Route path={route.editArticleId} element={<EditArticlePage />}/>
       </Route>
     </Routes>
   )
---
+++ src/helpers/helpers.js
@@ -0,0 +1,8 @@
+export const route = {
+    createAccount: 'create-account',
+    Login: 'login',
+    editAccount: 'edit-account',
+    Id: '/:id',
+    createArticle: 'create-article',
+    editArticleId: 'edit-article/:id',
+}
